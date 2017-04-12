65cfc142ce4c932feb6789bb846b253564fb2e78
Jeremy SCHOTTE <jschotte@e1r4p12.42.fr>
2017-04-12 18:23:07 +0200

    remove file

--- d06/ex02/jokes.ml
+++
@@ -1,9 +0,0 @@
-let () =
-    Random.self_init();
-    let jokes = [|"Quel super héros donne le plus vite l'heure ? -  Speed heure man (spiderman) !";
-                  "Ce matin, j'ai voulu faire une blague sur le Super U, mais elle n'a pas Supermarché !";
-                  "Quel est le comble pour un professeur de musique ? - Mettre des mauvaises notes";
-                  "Qu'est ce qui pleure quand ont lui tourne la tête ? - Un robinet";
-                  "J'ai cru que Mozart était mort mais mozzarella"|] 
-    in
-    print_endline jokes.(Random.int (Array.length jokes))
--- d06/ex04/sum.ml
+++
@@ -1,5 +0,0 @@
-let sum x y =
-    x +. y
-
-let () =
-    print_float (sum 42.0 42.0)
